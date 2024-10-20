5d86a17ed187faf3b759c16df84be9a899605f06
Yevgen Kotvitskyi <yevgeniy.kotvitskyi@gmail.com>
2024-10-20 17:51:34 +0300

    add custom storage

--- src/upload/storage.config.ts
+++ src/upload/storage.config.ts
@@ -1,4 +1,5 @@
 import { diskStorage } from 'multer';
+const fs = require('fs');
 
 export const storage = diskStorage({
   destination: './uploads/',
@@ -11,3 +12,38 @@ export const storage = diskStorage({
 const getFileName = (file: Express.Multer.File) => {
   return Date.now() + `__${file.originalname.replaceAll(' ', '_')}`;
 };
+
+function getDestination(req, file, cb) {
+  cb(null, '/dev/null');
+}
+
+function MyCustomStorage(opts) {
+  this.getDestination = opts.destination || getDestination;
+}
+
+MyCustomStorage.prototype._handleFile = function _handleFile(req, file, cb) {
+  this.getDestination(req, file, function (err, path) {
+    if (err) return cb(err);
+
+    var outStream = fs.createWriteStream(path);
+
+    file.stream.pipe(outStream);
+    outStream.on('error', cb);
+    outStream.on('finish', function () {
+      cb(null, {
+        path: path,
+        size: outStream.bytesWritten,
+      });
+    });
+  });
+};
+
+MyCustomStorage.prototype._removeFile = function _removeFile(req, file, cb) {
+  fs.unlink(file.path, cb);
+};
+
+export const customStorage = new MyCustomStorage({
+  destination: function (req, file, cb) {
+    cb(null, './uploads/' + file.originalname);
+  },
+});
--- src/upload/upload.controller.ts
+++ src/upload/upload.controller.ts
@@ -1,12 +1,15 @@
 import {
+  Body,
   Controller,
+  MaxFileSizeValidator,
+  ParseFilePipe,
   Post,
   UploadedFile,
   UseInterceptors,
 } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { Public } from 'src/app.controller';
-import { storage } from './storage.config';
+import { customStorage, storage } from './storage.config';
 
 @Controller('upload')
 export class UploadController {
@@ -15,5 +18,23 @@ export class UploadController {
   @UseInterceptors(FileInterceptor('file', { storage }))
   uploadFile(@UploadedFile() file: Express.Multer.File) {
     return 'success';
+  }
+
+  @Public()
+  @Post('/upload-validate')
+  @UseInterceptors(FileInterceptor('file', { storage: customStorage }))
+  async uploadFileAndPassValidation(
+    @Body() body: any,
+    @UploadedFile(
+      new ParseFilePipe({
+        validators: [new MaxFileSizeValidator({ maxSize: 10 * 1023 * 1024 })],
+      }),
+    )
+    file: Express.Multer.File,
+  ) {
+    return {
+      message: 'upload-validate success',
+      body,
+    };
   }
 }
